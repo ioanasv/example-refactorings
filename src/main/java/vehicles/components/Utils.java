b8c2e0649b16b84831e07168e7816c8e62e83ac3
Ioana Savu <56679711+ioanasv@users.noreply.github.com>
2020-05-25 15:56:51 +0200

    Update README.md  (#2)
    * Update README.md with refactorings that are not working.
    
    Co-authored-by: ioana <a.i.savu-1@student.tudelft.nl>

--- src/main/java/vehicles/components/Utils.java
+++ src/main/java/vehicles/components/Utils.java
@@ -8,4 +8,22 @@ public class Utils {
         return temp;
     }
 
+    public void parseString(String string) {
+        final boolean abc = string.toLowerCase().contains("abc");
+        final boolean b = string.lastIndexOf(".") > 5;
+        final boolean z = string.endsWith("z");
+        if (abc && b && z) {
+            string = string.replace("x.x", "");
+        } else {
+            return;
+        }
+    }
+
+    public static void print(int height, int width) {
+        final double perimeter = 2 * (height + width);
+        System.out.println(perimeter);
+        final double area = height * width;
+        System.out.println(area);
+    }
+
 }
