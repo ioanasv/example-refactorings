82b61233c28d625917ca14f01e682ee3481df82c
ioana <savuandreeaioana@yahoo.com>
2020-06-05 12:23:00 +0200

    Prepare code for multiple Extract Class op.

--- src/main/java/vehicles/birds/Bird.java
+++ src/main/java/vehicles/birds/Bird.java
@@ -5,6 +5,9 @@ public abstract class Bird extends Animalia {
     private String type;
     boolean isNailed;
     private double baseSpeed = 15.2;
+    private double maxSpeed = 50.5;
+    private String color;
+    private double weight;
 
     public Bird(String type, boolean isNailed) {
         this.type = type;
@@ -20,4 +23,33 @@ public abstract class Bird extends Animalia {
     double getLoadFactor() {
         return 1.3;
     }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setBaseSpeed(double baseSpeed) {
+        this.baseSpeed = baseSpeed;
+    }
 }
+
