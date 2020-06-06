fb60924a90fe7e09863cb2e23a6546cde79a0fe3
ioana <savuandreeaioana@yahoo.com>
2020-06-06 16:34:04 +0200

    Revert "Extract Attribute"
    This reverts commit 0e765363

--- src/main/java/vehicles/birds/Bird.java
+++ src/main/java/vehicles/birds/Bird.java
@@ -4,12 +4,8 @@ public abstract class Bird extends Animalia {
 
     private String type;
     boolean isNailed;
-
-
-
     private double baseSpeed = 15.2;
     private double maxSpeed = 50.5;
-    private final double loadfactor = 1.3;
 
     public Bird(String type, boolean isNailed) {
         this.type = type;
@@ -23,7 +19,7 @@ public abstract class Bird extends Animalia {
     }
 
     double getLoadFactor() {
-        return loadfactor;
+        return 1.3;
     }
 
     public double getMaxSpeed() {
